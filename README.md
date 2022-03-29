4314441a9dfc77e3778396a41ceccd9e4943db21
mizoc <mizoc@ymail.ne.jp>
2022-03-29 14:35:25 +0900

    :books: :sparkles: Update README, Add csv

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # logx
-TUI logging(in the meaning of ham radio) soft for linux  
-***Now, Developing...***
+***Now, Developing...***  
+Very Simple logging(in the meaning of ham radio) TUI script for Linux.  
+
+I prefer paper logs, but when I calling CQ, due to the large number of QSOs, I end up using this script.  
+So, Not recommended for everyday use by people who mainly use computer logs.  
 
 ## Installtion
 ```bash
+Install dialog
 $git clone https://github.com/mizoc/logx
+```
+
+## Usage
+```bash
+$./logx.sh FILE_SAVE_DESTINATION.csv
 ```
--- logx.sh
+++ logx.sh
@@ -20,19 +20,23 @@ test $# -ne 1 && {
   exit 1
 }
 OUT="$1"
-touch "$OUT"
+test -f "$OUT" || echo 'name,QTH,Remarks,Received RS(T),Sent RS(T),Call Sign,Start time,Frequency,Mode,End time' >"$OUT"
 
 TMP=`mktemp "/tmp/${0##*/}.tmp.XXXXXX"`
 trap 'rm "$TMP"; clear; exit 1' 1 2 3 15
 
 # Setting Mode, Freq.
-MODE=`dialog --stdout --menu "Mode" 11 30 5 CW "" SSB "" AM "" FM ""`
+MODE=`dialog --stdout --cancel-label "Exit" --menu "Mode" 11 30 5 CW "" SSB "" AM "" FM ""`
 STATUS=$?
 test $STATUS -ne 0 && kill -1 $$
 
-FRQ=`dialog --stdout --menu "Frq." 15 30 12 1.9MHz "" 3.5MHz "" 7MHz "" 10MHz "" 14MHz "" 18MHz "" 21MHz "" 28MHz "" 29MHz "" 50MHz "" 144MHz "" 433MHz ""`
-STATUS=$?
-test $STATUS -ne 0 && kill -1 $$
+# FRQ=`dialog --stdout --menu "Frq." 15 30 12 1.9MHz "" 3.5MHz "" 7MHz "" 10MHz "" 14MHz "" 18MHz "" 21MHz "" 28MHz "" 29MHz "" 50MHz "" 144MHz "" 433MHz ""`
+while :;do
+  FRQ=`dialog --stdout --cancel-label "Exit" --inputbox "Frequency:" 8 40`
+  STATUS=$?
+  test $STATUS -ne 0 && kill -1 $$
+  test -n "$FRQ" && break
+done
 
 # Logging...
 test "$MODE" = "CW" && DEFAULT_RST=599 || DEFAULT_RST=59
@@ -46,21 +50,27 @@ while :;do
   test -z "$CALL" && continue
 
   #Set start time
-  START_TIME=`date --utc '+%Y/%m/%d %H:%M'`
+  START_TIME=`date --utc '+%Y/%m/%d %H:%M(UTC)'`
 
   #Set remarks
   INFO=$(dialog --stdout --title "QSO Info" --cancel-label "Discard QSO" --form "" 20 60 16 \
-    "Remarks:" 1 1 "" 1 25 25 30 \
-    "Rcvd:" 2 1 "$DEFAULT_RST" 2 25 25 30 \
-    "Sent:" 3 1 "$DEFAULT_RST" 3 25 25 30 \
-    "Call Sign:" 4 1 "$CALL" 4 25 25 30 \
-    "Start Time:" 5 1 "$START_TIME" 5 25 25 30)
+    "Name:" 1 1 "" 1 25 25 30 \
+    "QTH:" 2 1 "" 2 25 25 30 \
+    "Remarks:" 3 1 "" 3 25 25 30 \
+    "Rcvd:" 4 1 "$DEFAULT_RST" 4 25 25 30 \
+    "Sent:" 5 1 "$DEFAULT_RST" 5 25 25 30 \
+    "Call Sign:" 6 1 "$CALL" 6 25 25 30 \
+    "Start Time:" 7 1 "$START_TIME" 7 25 25 30 \
+    "Frequency:" 8 1 "$FRQ" 8 25 25 30 \
+    "Mode:" 9 1 "$MODE" 9 25 25 30)
+  STATUS=$?
+  test $STATUS -ne 0 && continue
 
   #Set end time
-  END_TIME=`date --utc '+%Y/%m/%d %H:%M'`
+  END_TIME=`date --utc '+%Y/%m/%d %H:%M(UTC)'`
 
   #Write to the file
-  echo "$INFO" |tr "\n" '%' >>$OUT
+  echo "$INFO" |tr ',' '.' | tr "\n" ',' >>$OUT
   echo $END_TIME >>$OUT
 done
 
