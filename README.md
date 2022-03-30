1b96e9d615a94eafb01ba89117842ff065f94027
mizoc <mizoc@ymail.ne.jp>
2022-03-30 23:00:38 +0900

    Changed output of csv2adif.sh to stdout, Update README

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # loglogx
 Very Simple logging(in the meaning of ham radio) TUI script for Linux.  
 
-I prefer paper logs, but when I calling CQ, due to the large number of QSOs, I end up using this script.  
-So, Not recommended for everyday use by people who mainly use computer logs.  
+I prefer paper logs, but when I calling CQ, due to the large number of QSOs, I usually use this script.  
+ADIF format is supported. So, you can upload to eQSL.cc.  
 
 ## Installtion
 ```bash
-Install dialog
+First of all, install dialog
 $git clone https://github.com/mizoc/loglogx
 ```
 
 ## Usage
 ```bash
-$./loglogx.sh FILE_SAVE_DESTINATION.csv
-$./csv2adif.sh IN.csv OUT.adi  #When you want to change format to adif
+$./loglogx.sh SAVE_DESTINATION.csv
+$./csv2adif.sh IN.csv >OUT.adi  #When you want adif format
 ```
 ![demo](https://user-images.githubusercontent.com/47818505/160543535-7285091a-c507-49e5-a9be-7e673d290f9e.gif)
--- csv2adif.sh
+++ csv2adif.sh
@@ -4,38 +4,37 @@
 # Repository:https://github.com/mizoc/loglogx
 #
 # Usage
-#  $./csv2adif.sh IN.csv OUT.adi
-#      *OUT.adif must be empty
+#  $./csv2adif.sh IN.csv >OUT.adi
 #
 ########################################
 
-# Initial Setting...
-test ! -f "$1" -o -s "$2" -o $# -ne 2&& {
-  echo "Usage: $`basename $0` IN.csv OUT.adi" >&2
-  echo '  *OUT.adif must be empty.' >&2
+test ! -f "$1" -o $# -ne 1&& {
+  echo "Usage: $`basename $0` IN.csv" >&2
   exit 1
 }
 
-IN="$1"
-OUT="$2"
-cat >"$OUT" <<- END
-  Generated on `date --utc '+%Y-%m-%d'` at `date --utc '+%H:%M:%S'`Z by loglogx
-
-	<adif_ver:5>3.1.2
-	<programid:4>loglogx
-
+cat <<- END
+	Generated on `date --utc '+%Y-%m-%d'` at `date --utc '+%H:%M:%S'`Z by loglogx
+	<ADIF_VER:5>3.1.2
+	<CREATED_TIMESTAMP:15>`date --utc '+%Y%m%d %H%M%S'`
+	<PROGRAMID:4>loglogx
 	<EOH>
 END
 
-sed -e '1d' "$IN" | while read LINE;do
+sed -e '1d' "$1" | while read LINE;do
   echo $LINE | awk -F , '{
+  print ""
   gsub("/", "", $7); print "<QSO_DATE:8>" $7
   gsub(":", "", $8); print "<TIME_ON:"length($8)">" $8
+  gsub("/", "", $11); print "<QSO_DATE_OFF:8>" $11
+  gsub(":", "", $12); print "<TIME_OFF:"length($12)">" $12
   print "<CALL:" length($6) ">" $6
   print "<FREQ:" length($9) ">" $9
   print "<MODE:" length($10) ">" $10
   print "<RST_RCVD:" length($4) ">" $4
   print "<RST_SENT:" length($5) ">" $5
-  print "<EOR>"
-  }' >>"$OUT"
+  print "<QTH:" length($2) ">" $2
+  print "<NAME:" length($1) ">" $1
+  print "<COMMENT:" length($3) ">" $3
+  }'
 done
